8492277085a5bf018f5ec6c7fc8c7cde2ca0d659
Godbless Biekro <biekrogodbless@gmail.com>
2023-03-02 09:19:24 +0000

    updated codes

--- rot.c
+++ rot.c
@@ -4,7 +4,6 @@ char *rot13(char *str);
 int main(void)
 {
 
-
     char s[] = "ROT13 (\"rotate by 13 places\", sometimes hyphenated ROT-13) is a simple letter substitution cipher.\n";
     char *p;
 
