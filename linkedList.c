660f76f9099fcdbfd6b49f5ec9c53e7a82a8adc8
Godbless Biekro <biekrogodbless@gmail.com>
2023-03-29 21:00:14 +0000

    linked list added

---
+++ linkedList.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**Declarre struct for the node*/
+struct node
+{
+    int data;
+    struct node *link;
+};
+
+void count_of_nodes(struct node *head);
+void print_data(struct node * head);
+
+int main(void)
+{
+  struct node *head, *current;
+
+  /**CReate first node*/
+  head = malloc(sizeof(struct node));
+  head->data = 45;
+  head->link = NULL;
+
+  /**creating second node*/
+  current = malloc(sizeof(struct node));
+  current->data = 98;
+  current->link = NULL;
+
+  /**Link the two nodes*/
+  head->link = current;
+
+  /**Create third node*/
+  current = malloc(sizeof(sizeof(struct node)));
+  current->data = 3;
+  current->link = NULL;
+
+  /**connect this third node to second node*/
+  head->link->link = current;
+
+   count_of_nodes(head);
+   print_data(head);
+    return 0;
+}
+void count_of_nodes(struct node *head)
+{
+    int count = 0;
+    struct node *ptr= NULL;
+
+
+    if (head == NULL)
+        printf("The linked List is Empty");
+    
+    ptr = head;
+
+    while(ptr != NULL)
+    {
+        count++;
+        ptr = ptr->link;
+    }
+    printf("%d\n", count);
+}
+
+void print_data(struct node *head)
+{
+    if (head == NULL)
+        printf("THe linked list is empty");
+
+    struct node *ptr = head;
+
+    while(ptr != NULL)
+    {
+        printf("%d ", ptr->data);
+        ptr = ptr->link;
+    }
+    printf("\n");
+}
+
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,1 +1,11 @@
-accept
+
+  /**Create third node*/
+  current = malloc(sizeof(sizeof(struct node)));
+  current->data = 3;
+  current->link = NULL;
+
+  /**connect this third node to second node*/
+  head->link->link = current;
+
+   count_of_nodes(head);
+   print_dat
